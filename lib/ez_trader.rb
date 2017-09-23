f89e3e7ac66ea81957ba4e9cfeffb7f9d8189dbe
Makoto Saito <mlsaito@live.com>
2017-09-23 22:54:00 +0900

    Get non-zero balance from Bittrex Wallet only.

--- lib/ez_trader.rb
+++ lib/ez_trader.rb
@@ -11,8 +11,20 @@ Bittrex.config do |c|
   c.secret = ENV['BITTREX_SECRET_KEY']
 end
 
-my_wallet = Bittrex::Wallet.all
-puts "Your current wallet: #{my_wallet}"
+# Extract non-zero balance 
+def get_non_zero_wallet(my_wallet)
+  my_wallet.delete_if do |coin|
+    if coin.balance == 0
+      true
+    else
+      false
+    end
+  end
+  my_wallet
+end
+
+my_wallet = get_non_zero_wallet(Bittrex::Wallet.all)
+puts "Your current wallet: #{my_wallet}\n"
 
 # Calls Bittrex API per minute
 loop do
